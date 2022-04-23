97f09226845b14d8091fbf29a2829818422a2b9d
Melody Kao <melody.ch.kao@gmail.com>
2022-04-23 10:56:09 +0200

    Test Atatus Setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('41c2ffc82a3142b4bf9e5604d9216098').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +23,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
