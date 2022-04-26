7cf4b6b1362adf4747eb95b6412cd6c1896d6efa
Melody Kao <melody.ch.kao@gmail.com>
2022-04-26 11:07:50 +0200

    Updates

---
+++ precache-manifest.4dc1dcf5c3aeefb519b3e79b8bd8d7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fdb461aadb87bbebce4",
+    "url": "/Meet/static/css/main.ccbea31c.chunk.css"
+  },
+  {
+    "revision": "9fdb461aadb87bbebce4",
+    "url": "/Meet/static/js/main.6de78b0e.chunk.js"
+  },
+  {
+    "revision": "a22f7f672cc0fd4b81c9",
+    "url": "/Meet/static/js/runtime~main.148cb5b1.js"
+  },
+  {
+    "revision": "65e8f55b72e5d3f14014",
+    "url": "/Meet/static/js/2.ecefa9f3.chunk.js"
+  },
+  {
+    "revision": "b10fc4df4216b03278df",
+    "url": "/Meet/static/js/3.af9a8324.chunk.js"
+  },
+  {
+    "revision": "4f829730147021645731730ffeb7773b",
+    "url": "/Meet/index.html"
+  }
+];
